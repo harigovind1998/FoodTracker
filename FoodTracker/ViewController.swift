2ac5dd2686685a1857baaa68d8284e240d6a4c72
Hari Govind <grhari1998@gmail.com>
2019-08-15 23:20:38 -0400

    Initial commit for FoodTracking learning app for xCode

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Hari Govind on 2019-08-15.
+//  Copyright © 2019 Hari Govind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
